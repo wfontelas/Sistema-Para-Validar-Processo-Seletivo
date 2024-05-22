b7e783ead6d57bb0e9c6db666cd3a2c4b8e7e2d4
William Fontelas <williamdevsuporte@gmail.com>
2024-05-22 18:49:21 -0300

    Criado os comentarios sobre os cases

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -12,6 +12,13 @@ public class ProcessoSeletivo {
          }
     }
 
+
+
+    /*
+    * Case 4: O RH deverá realizar uma ligação, com no máximo 03 tentativas para cada candidato selecionado e caso o candidato
+    *  atenda, deve-se imprimir:
+        -"CONSEGUIMOS CONTATO COM _ [CANDIDATO]``` APÓS **_**[TENTATIVA]`** TENTATIVA(S)" ;**
+        -Do contrário imprima: "NÃO CONSEGUIMOS CONTATO COM O _[CANDIDATO]_".*/
     static void entrandoEmContato(String candidato){
         int tentativasRealizadas = 1;
         boolean continuarTentando = true;
@@ -28,9 +35,10 @@ public class ProcessoSeletivo {
         }while(continuarTentando && tentativasRealizadas < 3);
 
         if(atendeu)
-            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "º TENTATIVA");
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "º " + "TENTATIVA");
         else
-            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS " +
+                    tentativasRealizadas + " REALIZADA");
 
     }
 
@@ -39,6 +47,8 @@ public class ProcessoSeletivo {
         return new Random().nextInt(3) == 1;
     }
 
+    /*
+    * Case 3: Agora é hora de imprimir a lista dos candidatos selecionados, para disponibilizar para o RH entrar em contato.*/
     static void imprimirSelecionados() {
         String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
         System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
@@ -53,9 +63,14 @@ public class ProcessoSeletivo {
         }
     }
 
+    /*
+    * Case 2: Foi solicitado, que nosso sistema garanta que, diante das inúmeras candidaturas sejam selecionados apenas no máximo,
+    *  5 candidatos para entrevista, onde o salário pretendido seja menor ou igual ao salário base.*/
     static void selecaoCandidatos() {
-        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA",
-                "DANIELA", "JORGE"};
+
+        // Array com a lista de candidatos
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
         int candidatosSelecionados = 0;
         int candidatoAtual = 0;
         double salarioBase = 2000.0;
@@ -75,10 +90,21 @@ public class ProcessoSeletivo {
         }
     }
 
+    // Método que simula o valor pretendido
     static double valorPretendido(){
         return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
 
+
+
+    /*
+    Case 1: Vamos imaginar que em um processo seletivo, existe o valor base salarial de R$ 2.000,00 e o salário pretentido pelo
+    candidato. Vamos elaborar um controle de fluxo onde:
+        -Se o valor salário base, for maior que valor salário pretentido, imprima : LIGAR PARA O CANDIDATO;
+        -Senão, Se o valor salário base for igual ao valor salário pretentido, imprima : LIGAR PARA O CANDIDATO, COM CONTRA
+        PROPOSTA;
+        -Senão imprima: AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS.
+    * */
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido){
