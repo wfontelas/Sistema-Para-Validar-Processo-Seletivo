d78cf28619d3a8e968d9466e94fb683a955b04f3
William Fontelas <williamdevsuporte@gmail.com>
2024-05-22 00:20:42 -0300

    Criado os metodos entrandoEmContato e atender

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,42 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-            imprimirSelecionados();
+         String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+         //primeiro um for para selecionar os candidatos
+         for (String candidato : candidatos){
+             entrandoEmContato(candidato);
+         }
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do{
+           atendeu = atender();
+           continuarTentando = !atendeu;
+           if(continuarTentando){
+               tentativasRealizadas++;
+           }else {
+               System.out.println("CONTATO REALIZADO COM SUCESSO");
+           }
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "º TENTATIVA");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+
+    }
+
+    //método auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     static void imprimirSelecionados() {
